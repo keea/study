402ea6d6db11382659089bc145b895d32c3ab265
heea <heea44@naver.com>
2017-03-30 10:41:53 +0900

    전역변수와 동일한 이름의 지역변수가 선언된다면

---
+++ LocalValHideGlobal/LocalValHideGlobal/source.cpp
@@ -0,0 +1,22 @@
+/*
+* 열혈 C프로그래밍 p231
+* 전역변수와 동일한 이름의 지역변수가 선언된다면
+* @date     Tue Mar 28 11:14:37 2017
+* @author   강희아
+*/
+
+#include <stdio.h>
+int Add(int val);
+int num = 1;
+
+void main() {
+	int num = 5;
+	printf("num: %d \r\n", Add(3));
+	printf("num: %d \r\n", num + 9);
+}
+
+int Add(int val) {
+	int num = 9;
+	num += val;
+	return num;
+}
