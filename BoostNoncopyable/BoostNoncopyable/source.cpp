0f8575cce5fc30046f9282d6b8e120e122ae2d60
keea <heea44@naver.com>
2017-09-27 17:09:57 +0900

    Boost Noncopyable

---
+++ BoostNoncopyable/BoostNoncopyable/source.cpp
@@ -0,0 +1,52 @@
+/*
+* Boost Noncopyable
+* @file		source.cpp
+* @date     Wed Sep 27 16:36:45 2017
+* @author   keea
+*/
+
+#include <iostream>
+#include <assert.h>
+#include <conio.h>
+
+class Uncopyable {
+protected:
+	//파생된 클래스에 대해 생성과 소멸을 허용.
+	Uncopyable() {};
+	~Uncopyable() {};
+
+private:
+	Uncopyable(const Uncopyable&);
+	Uncopyable& operator=(const Uncopyable&);
+};
+
+class K_Class : Uncopyable {
+private:
+	int m_i;
+
+	//링크가 안됨.
+	//K_Class(const K_Class&); //private으로 선언하고,
+	//K_Class& operator=(const K_Class&); // 구현을 정의하지 않음.
+public:
+	K_Class(int i = 0) :m_i(i) {}
+
+	//컴파일이나 링크할 때 에러가 남.
+	/*K_Class(const K_Class & other) {
+		assert(!"Do not use copy constructor.");
+	}
+	K_Class& operator=(const K_Class& other) {
+		assert(!"Do not use copy assignment.");
+		return *this;
+	}*/
+};
+
+
+void main() {
+	K_Class kc1;
+	//K_Class kc2(kc1); //복사 생성자 호출
+
+	K_Class * pkc = new K_Class;
+
+	//kc1 = *pkc; //복사 대입 연산자 호출
+	_getch();
+}
